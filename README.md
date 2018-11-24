be4348767b9146a06a593e430b20b8d1fa467976
Andrew Spriggs <spriggsy83@yahoo.com.au>
2018-11-24 19:43:06 +1100

    Started playing with express.js

---
+++ README.md
@@ -0,0 +1,5 @@
+Running api from Windows:
+```
+cd aqua2-express-api
+set DEBUG=aqua2-express-api:* & npm start`
+```
---
+++ aqua2-express-api/app.js
@@ -0,0 +1,41 @@
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
+var mysql = require("mysql");
+
+var indexRouter = require('./routes/index');
+var samplesRouter = require('./routes/samples');
+
+var app = express();
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+/*app.use(function(req, res, next){
+	res.locals.connection = mysql.createConnection({
+		host     : 'cowpea.it.csiro.au',
+		user     : 'sails',
+		password : 'sailsDBpw',
+		database : 'annotQDB'
+	});
+	res.locals.connection.connect();
+	next();
+});*/
+
+var dbpool = mysql.createPool({
+	connectionLimit : 10,
+	host     : 'cowpea.it.csiro.au',
+	user     : 'sails',
+	password : 'sailsDBpw',
+	database : 'annotQDB'
+});	
+global.dbpool = dbpool;
+
+app.use('/', indexRouter);
+app.use('/api/v1/samples', samplesRouter);
+
+module.exports = app;
