83310b090a0cf60832579ecb646d2476e12f7691
Andrew Spriggs <spriggsy83@yahoo.com.au>
2018-11-29 00:55:21 +1100

    Added API Swagger doc

--- aqua2-express-api/app.js
+++ aqua2-express-api/app.js
@@ -3,6 +3,9 @@ var cors = require("cors");
 var path = require("path");
 var cookieParser = require("cookie-parser");
 var logger = require("morgan");
+var swaggerUi = require('swagger-ui-express');
+var YAML = require('yamljs');
+var swaggerDocument = YAML.load('./docs/swagger.yaml');
 
 var indexRouter = require("./routes/index");
 var samplesRouter = require("./routes/samples");
@@ -17,8 +20,10 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
 
+app.use('/api2-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+
 app.use("/", indexRouter);
-app.use("/api/v1/samples", samplesRouter);
-app.use("/api/v1/sequences", sequencesRouter);
+app.use("/api/v2/samples", samplesRouter);
+app.use("/api/v2/sequences", sequencesRouter);
 
 module.exports = app;
