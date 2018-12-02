f5c48d724799b0c08663784af76014b0f59415b4
Andrew Spriggs <spriggsy83@yahoo.com.au>
2018-12-03 10:33:16 +1100

    Redux store working for Samples table

--- aqua2-react-fe/src/components/Samples.js
+++ aqua2-react-fe/src/components/Samples.js
@@ -1,10 +1,7 @@
 import React, { Component } from "react";
-//import axios from "axios";
-//import API from "../API";
 import MuiDataTable from "mui-datatables";
-import MaterialTable from "material-table";
 import LinearProgress from "@material-ui/core/LinearProgress";
-//import { map, pick, values } from "lodash";
+import { map, pick, values } from "lodash";
 import { connect } from "react-redux";
 import { getSamples } from "../actions/samples_actions.js";
 
@@ -13,10 +10,6 @@ class ListSamples extends Component {
 		super();
 		this.state = {
 			loaded: false,
-			page: 0,
-			total: 0,
-			rowsPerPage: 50,
-			orderby: null,
 			samples: []
 		};
 	}
@@ -28,71 +21,16 @@ class ListSamples extends Component {
 	// Get initial data
 	getData = () => {
 		this.props.getSamples();
-		/*const { page, rowsPerPage } = this.state;
-		const offset = page * rowsPerPage;
-		API.get(`samples?limit=${rowsPerPage}&offset=${offset}`).then(res => {
-			const samples = map(res.data.data, sample => {
-				return values(
-					pick(sample, [
-						"id",
-						"name",
-						"species",
-						"description",
-						"ingroups",
-						"numseqs"
-					])
-				);
-			});
-			this.setState({ samples, total: res.data.total });
-		});*/
 	};
 
 	render() {
-		const { total, samples, loaded } = this.props;
-		/*		const { page, rowsPerPage } = this.state;
+		const { samples, loaded } = this.props;
 		const options = {
-			pagination: true,
+			pagination: false,
 			viewColumns: false,
 			selectableRows: false,
 			filter: false,
-			rowsPerPageOptions: [50, 100, 200],
-			rowsPerPage: rowsPerPage,
-			page: page,
-			count: total,
-			serverSide: true,
-			onTableChange: (action, tableState) => {
-				if (
-					tableState.page !== page ||
-					tableState.rowsPerPage !== rowsPerPage
-				) {
-					this.setState(
-						{
-							page: tableState.page,
-							rowsPerPage: tableState.rowsPerPage
-						},
-						() => {
-							this.getData();
-						}
-					);
-				}
-			},
-			onColumnSortChange: (changedColumn, direction) => {
-				var col = changedColumn
-					.replace("Sample Name", "name")
-					.replace("Species", "species")
-					.replace("In groups/assemblies", "ingroups")
-					.replace("Num. sequences", "numseqs");
-				var dir = direction.replace(/(asc|desc)ending/, "$1");
-				this.setState(
-					{
-						orderby: `${col} ${dir}`,
-						page: 0
-					},
-					() => {
-						this.getData();
-					}
-				);
-			}
+			rowsPerPage: 999
 		};
 		const columns = [
 			{ name: "dbID", options: { display: "excluded" } },
@@ -118,44 +56,16 @@ class ListSamples extends Component {
 				}
 			}
 		];
-*/
-		const columns = [
-			{ title: "dbID", field: "id", hidden: true },
-			{ title: "Sample Name", field: "name" },
-			{ title: "Species", field: "species" },
-			{ title: "Description", field: "description" },
-			{
-				title: "In groups/assemblies",
-				field: "ingroups",
-				type: "numeric",
-				render: rowData => {
-					return <div>{rowData.ingroups.toLocaleString()}</div>;
-				}
-			},
-			{
-				title: "Num. sequences",
-				field: "numseqs",
-				type: "numeric",
-				render: rowData => {
-					return <div>{rowData.numseqs.toLocaleString()}</div>;
-				}
-			}
-		];
 
 		if (loaded) {
 			return (
 				<div>
-					<MaterialTable
-						columns={columns}
-						data={samples}
-						title="Samples"
-					/>
-					{/*<MuiDataTable
+					<MuiDataTable
 						data={samples}
 						columns={columns}
 						options={options}
 						title={"Samples"}
-					/>*/}
+					/>
 				</div>
 			);
 		} else {
@@ -176,10 +86,24 @@ class ListSamples extends Component {
  * allows us to call our application state from props
  */
 function mapStateToProps(state) {
+	var samples = [];
+	if (state.samples.samples.length) {
+		samples = map(state.samples.samples, sample => {
+			return values(
+				pick(sample, [
+					"id",
+					"name",
+					"species",
+					"description",
+					"ingroups",
+					"numseqs"
+				])
+			);
+		});
+	}
 	return {
 		loaded: state.samples.loaded,
-		total: state.samples.total,
-		samples: state.samples.samples
+		samples: samples
 	};
 }
 
