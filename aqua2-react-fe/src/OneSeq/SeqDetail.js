074a2f6682f31ea5289191ddbda81afd9689e971
Andrew Spriggs <spriggsy83@yahoo.com.au>
2019-03-07 17:15:06 +1100

    Added interface to edit seq annotations

--- aqua2-react-fe/src/OneSeq/SeqDetail.js
+++ aqua2-react-fe/src/OneSeq/SeqDetail.js
@@ -1,41 +1,65 @@
-import React, { Component } from "react";
-import { withRouter } from "react-router-dom";
-import { connect } from "react-redux";
-import compose from "recompose/compose";
-import { withStyles } from "@material-ui/core/styles";
-import { createStructuredSelector } from "reselect";
-import Paper from "@material-ui/core/Paper";
-import Typography from "@material-ui/core/Typography";
-import List from "@material-ui/core/List";
-import ListItem from "@material-ui/core/ListItem";
-import ListItemText from "@material-ui/core/ListItemText";
-import ExpansionPanel from "@material-ui/core/ExpansionPanel";
-import ExpansionPanelDetails from "@material-ui/core/ExpansionPanelDetails";
-import ExpansionPanelSummary from "@material-ui/core/ExpansionPanelSummary";
-import ExpandMoreIcon from "@material-ui/icons/ExpandMore";
-import { Link } from "react-router-dom";
-import { renderLoadingBars, UnderConstruction } from "../common/renderHelpers";
-import { requestOneSeq } from "./oneseq_actions";
-import * as selectors from "./oneseq_selectors";
-import { SequenceViewer } from "./SeqString";
+import React, { Component } from 'react';
+import { withRouter } from 'react-router-dom';
+import { connect } from 'react-redux';
+import compose from 'recompose/compose';
+import { withStyles } from '@material-ui/core/styles';
+import { createStructuredSelector } from 'reselect';
+import Paper from '@material-ui/core/Paper';
+import Typography from '@material-ui/core/Typography';
+import List from '@material-ui/core/List';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemText from '@material-ui/core/ListItemText';
+import ExpansionPanel from '@material-ui/core/ExpansionPanel';
+import ExpansionPanelDetails from '@material-ui/core/ExpansionPanelDetails';
+import ExpansionPanelSummary from '@material-ui/core/ExpansionPanelSummary';
+import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
+import Button from '@material-ui/core/Button';
+import EditIcon from '@material-ui/icons/Edit';
+import OkIcon from '@material-ui/icons/Check';
+import UndoIcon from '@material-ui/icons/Undo';
+import TextField from '@material-ui/core/TextField';
+import { Link } from 'react-router-dom';
+import { renderLoadingBars, UnderConstruction } from '../common/renderHelpers';
+import { requestOneSeq, patchOneSeq } from './oneseq_actions';
+import * as selectors from './oneseq_selectors';
+import { SequenceViewer } from './SeqString';
 
-const styles = theme => ({
-	narrowlist: {
-		width: 500,
+const styles = (theme) => ({
+	root: {
 		paddingTop: theme.spacing.unit * 2,
 		paddingBottom: theme.spacing.unit * 2,
 		paddingLeft: theme.spacing.unit * 2,
-		paddingRight: theme.spacing.unit * 2
+		paddingRight: theme.spacing.unit * 2,
 	},
-	rightjust: { display: "flex", justifyContent: "flex-end" }
+	narrowlist: {
+		width: 500,
+	},
+	rightjust: {
+		display: 'flex',
+		justifyContent: 'flex-end',
+	},
+	margin: {
+		margin: theme.spacing.unit,
+	},
+	rightMargin: {
+		marginRight: theme.spacing.unit,
+	},
+	textField: {
+		flexGrow: 1,
+	},
 });
 
 class SeqDetail extends Component {
 	state = {
-		expanded: null
+		expanded: null,
+		editAnnot: false,
+		draftAnnotNote: '',
 	};
 
 	componentDidMount() {
+		// Required to capture Escape key press
+		window.addEventListener('keydown', this.textEditKeyPress);
+
 		var seqName = this.props.match.params.sequence_name;
 		var subseqStart = null;
 		var subseqEnd = null;
@@ -54,7 +78,7 @@ class SeqDetail extends Component {
 			this.props.requestOneSeq({
 				name: seqName,
 				subseqStart: subseqStart,
-				subseqEnd: subseqEnd
+				subseqEnd: subseqEnd,
 			});
 		} else if (
 			subseqStart !== this.props.subseqStart ||
@@ -65,20 +89,24 @@ class SeqDetail extends Component {
 		}
 	}
 
+	componentWillUnmount() {
+		window.removeEventListener('keydown', this.textEditKeyPress);
+	}
+
 	renderSeqList = () => {
 		const { seqDetailTab, classes } = this.props;
 		return (
 			<List>
 				{seqDetailTab.map(function(field, index) {
 					var [fieldLabel, fieldValue, fieldLink] = field;
-					if (fieldLabel === "External link") {
+					if (fieldLabel === 'External link') {
 						return (
 							<a
 								href={fieldLink}
 								target="_blank"
-								style={{ textDecoration: "none" }}
+								style={{ textDecoration: 'none' }}
 								rel="noopener noreferrer"
-								key={index + "-extlink"}
+								key={index + '-extlink'}
 							>
 								<ListItem button>
 									<ListItemText primary={fieldLabel} />
@@ -93,8 +121,8 @@ class SeqDetail extends Component {
 						return (
 							<Link
 								to={fieldLink}
-								style={{ textDecoration: "none" }}
-								key={index + "-link"}
+								style={{ textDecoration: 'none' }}
+								key={index + '-link'}
 							>
 								<ListItem button>
 									<ListItemText primary={fieldLabel} />
@@ -105,9 +133,9 @@ class SeqDetail extends Component {
 								</ListItem>
 							</Link>
 						);
-					} else {
+					} else if (fieldLabel !== 'Annotation/note') {
 						return (
-							<ListItem key={index + "-listitem"}>
+							<ListItem key={index + '-listitem'}>
 								<ListItemText primary={fieldLabel} />
 								<ListItemText
 									className={classes.rightjust}
@@ -115,15 +143,123 @@ class SeqDetail extends Component {
 								/>
 							</ListItem>
 						);
+					} else {
+						return null;
 					}
 				})}
 			</List>
 		);
 	};
 
-	handleSectionChange = panel => (event, expanded) => {
+	toggleEditAnnot = () => {
+		const { editAnnot } = this.state;
+		const { seqDetail } = this.props;
+		const seqAnnot = seqDetail.annotNote || '';
+		if (editAnnot) {
+			this.setState({
+				editAnnot: false,
+			});
+		} else {
+			this.setState({
+				editAnnot: true,
+				draftAnnotNote: seqAnnot,
+			});
+		}
+		return;
+	};
+
+	textEditKeyPress = (event) => {
+		const { editAnnot } = this.state;
+		if (editAnnot) {
+			if (event.key === 'Enter') {
+				this.submitNewAnnotNote();
+			} else if (event.key === 'Escape') {
+				this.setState({ editAnnot: false });
+			}
+		}
+		return;
+	};
+
+	handleAnnotTextChange = (event) => {
+		this.setState({ draftAnnotNote: event.target.value });
+	};
+
+	submitNewAnnotNote = () => {
+		const { draftAnnotNote } = this.state;
+		const { seqDetail } = this.props;
+		const seqAnnot = seqDetail.annotNote || '';
+		if (seqAnnot !== draftAnnotNote) {
+			this.props.patchOneSeq({
+				id: this.props.seqID,
+				annotNote: this.state.draftAnnotNote,
+			});
+		}
+		this.setState({ editAnnot: false });
+	};
+
+	renderAnnotNotes = () => {
+		const { seqDetail, classes } = this.props;
+		const seqAnnot = seqDetail.annotNote || '';
+		const { editAnnot, draftAnnotNote } = this.state;
+
+		if (editAnnot) {
+			return (
+				<ListItem>
+					<ListItemText primary="Annotation/note" />
+					<TextField
+						className={classes.textField}
+						id="annot-text"
+						value={draftAnnotNote}
+						inputProps={{ maxLength: 255 }}
+						margin="normal"
+						autoFocus
+						onChange={(e) => this.handleAnnotTextChange(e)}
+					/>
+					<Button
+						variant="contained"
+						aria-label="Submit"
+						size="small"
+						className={classes.margin}
+						onClick={(e) => this.submitNewAnnotNote()}
+					>
+						<OkIcon className={classes.rightMargin} />
+						Submit
+					</Button>
+					<Button
+						variant="contained"
+						aria-label="Submit"
+						size="small"
+						className={classes.margin}
+						onClick={(e) => this.toggleEditAnnot()}
+					>
+						<UndoIcon className={classes.rightMargin} />
+						Cancel
+					</Button>
+				</ListItem>
+			);
+		} else {
+			return (
+				<ListItem onClick={(e) => this.toggleEditAnnot()}>
+					<ListItemText primary="Annotation/note" />
+					<ListItemText primary={seqAnnot} />
+					<Button
+						variant="contained"
+						size="small"
+						aria-label="Edit"
+						className={classes.margin}
+						onClick={(e) => this.toggleEditAnnot()}
+					>
+						<EditIcon className={classes.rightMargin} />
+						Edit
+					</Button>
+				</ListItem>
+			);
+		}
+	};
+
+	handleSectionChange = (panel) => (event, expanded) => {
 		this.setState({
-			expanded: expanded ? panel : false
+			expanded: expanded ? panel : false,
 		});
 	};
 
@@ -132,36 +268,36 @@ class SeqDetail extends Component {
 		return (
 			<>
 				<ExpansionPanel
-					expanded={expanded === "seqstring"}
-					onChange={this.handleSectionChange("seqstring")}
+					expanded={expanded === 'seqstring'}
+					onChange={this.handleSectionChange('seqstring')}
 				>
 					<ExpansionPanelSummary expandIcon={<ExpandMoreIcon />}>
 						<Typography variant="h6">Sequence viewer</Typography>
 					</ExpansionPanelSummary>
 					<ExpansionPanelDetails>
-						{expanded === "seqstring" && <SequenceViewer />}
+						{expanded === 'seqstring' && <SequenceViewer />}
 					</ExpansionPanelDetails>
 				</ExpansionPanel>
 				<ExpansionPanel
-					expanded={expanded === "annottab"}
-					onChange={this.handleSectionChange("annottab")}
+					expanded={expanded === 'annottab'}
+					onChange={this.handleSectionChange('annottab')}
 				>
 					<ExpansionPanelSummary expandIcon={<ExpandMoreIcon />}>
 						<Typography variant="h6">Annotations table</Typography>
 					</ExpansionPanelSummary>
 					<ExpansionPanelDetails>
-						{expanded === "annottab" && <UnderConstruction />}
+						{expanded === 'annottab' && <UnderConstruction />}
 					</ExpansionPanelDetails>
 				</ExpansionPanel>
 				<ExpansionPanel
-					expanded={expanded === "hierarchyviz"}
-					onChange={this.handleSectionChange("hierarchyviz")}
+					expanded={expanded === 'hierarchyviz'}
+					onChange={this.handleSectionChange('hierarchyviz')}
 				>
 					<ExpansionPanelSummary expandIcon={<ExpandMoreIcon />}>
 						<Typography variant="h6">Relationship viewer</Typography>
 					</ExpansionPanelSummary>
 					<ExpansionPanelDetails>
-						{expanded === "hierarchyviz" && <UnderConstruction />}
+						{expanded === 'hierarchyviz' && <UnderConstruction />}
 					</ExpansionPanelDetails>
 				</ExpansionPanel>
 			</>
@@ -169,14 +305,17 @@ class SeqDetail extends Component {
 	};
 
 	render() {
-		const { seqName, loading, hasloaded, errorMsg, classes } = this.props;
+		const { loading, hasloaded, errorMsg, classes } = this.props;
 		return (
 			<>
-				<Paper className={classes.narrowlist} elevation={1}>
-					<Typography variant="h6">{seqName}:</Typography>
+				<Paper className={classes.root} elevation={1}>
+					<Typography variant="h6">Sequence detail:</Typography>
 					{loading && renderLoadingBars()}
 					{errorMsg && <Typography>{errorMsg}</Typography>}
-					{hasloaded && this.renderSeqList()}
+					<div className={classes.narrowlist}>
+						{hasloaded && this.renderSeqList()}
+					</div>
+					{hasloaded && this.renderAnnotNotes()}
 					{loading && renderLoadingBars()}
 				</Paper>
 				{hasloaded && this.renderFeatureAccordion()}
@@ -195,7 +334,7 @@ const mapStateToProps = createStructuredSelector({
 	seqID: selectors.getSeqID,
 	seqName: selectors.getSeqName,
 	seqDetail: selectors.getSeqDetail,
-	seqDetailTab: selectors.getSeqDetailTable
+	seqDetailTab: selectors.getSeqDetailTable,
 });
 
 /**
@@ -206,6 +345,6 @@ export default compose(
 	withStyles(styles),
 	connect(
 		mapStateToProps,
-		{ requestOneSeq }
-	)
+		{ requestOneSeq, patchOneSeq },
+	),
 )(SeqDetail);
